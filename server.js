1e7331c8804bb53db06c714d2951fd7929937428
Simon Cooper <kidney9@hotmail.com>
2020-12-01 12:10:13 +0000

    helmet.js used for noSniff only

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Set security headers
-app.use(helmet());
+app.use(helmet.noSniff());
 app.use((req, res, next) => {
   res.setHeader('X-XSS-Protection', '1; mode=block');
   res.setHeader('surrogate-control', 'no-store');
